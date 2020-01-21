db073f77ea1b89b454d5b6bc3b4e6f1abd14f975
kevin <uehara.kevin@gmail.com>
2020-01-20 22:21:50 -0300

    feat: implements the initial job to send email

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,9 +1,11 @@
+import Queue from '../lib/Queue';
+
 export default {
     async store(req, res) {
         const { name, email, password} = req.body;
         const user = { name, email, password };
 
-        // Add job
+        await Queue.add({ user });
 
         return res.json(user);
     }
